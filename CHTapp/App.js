b1a3af8ad6f8e2a6b05f43f61d995c7af4084e6b
TranChinh03 <21520166@gm.uit.edu.vn>
2023-04-27 14:38:57 +0700

    responsive + LoadingScreen

---
+++ CHTapp/App.js
@@ -0,0 +1,9 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import LoadingScreen from './screens/LoadingScreen'
+
+export default function App() {
+  return (
+      <LoadingScreen/>
+  )
+}
